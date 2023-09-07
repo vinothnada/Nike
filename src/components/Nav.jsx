c0c16439b16746fce89dbd6a40a0efd519f5c819
vinothnada <vinonada@gmail.com>
2023-09-07 11:46:16 +0530

    Nav and Hero layout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
@@ -9,8 +10,22 @@ const Nav = () => {
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          Home About Us Products Contact Us Sign in
+          {navLinks.map((item) => {
+            return (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
+              </li>
+            );
+          })}
         </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt={hamburger} width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
